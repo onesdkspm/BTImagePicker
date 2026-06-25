b3d7136097974221e4bc22804413a4c9c29f1379
m2构造机 <zhanggaoqi@aobi.com>
2026-06-25 16:12:55 +0800

    [Branch Release] v1.2.1-dev-1472010 - develop
    - 更新所有 XCFrameworks 到 1.2.1-dev-1472010
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -32,8 +32,8 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "BaitianImagePicker",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/BTImagePicker/1.2.1-dev-1471971/BaitianImagePicker.xcframework.zip",
-            checksum: "a95fe2d6d3128f91583201e5f211c89ce4fe7f600f5a5df56b8704321ac99761"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/BTImagePicker/1.2.1-dev-1472010/BaitianImagePicker.xcframework.zip",
+            checksum: "15acf16845c4b32f6fc33bf7957ca690deaab7f9f2a988a603cb9235e14ce8eb"
         ),
         
         // ========== Bundle Resources ==========
